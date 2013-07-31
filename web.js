762c3a40de76a537f238c6d76c8f52e33db77091
Ubuntu <ubuntu@ip-172-31-36-188.us-west-2.compute.internal>
2013-07-31 05:03:45 +0000

    Try 1

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var fileData = new Buffer(fs.readFileSync('./index.html', 'utf8'));
+var fileData = fs.readFileSync('./index.html', 'utf8');
 //var fileData = new Buffer('Something else');
 
 app.get('/', function(request, response) {
