e5df337038ae76b1f0c8b4b13eac578edf1e1139
Ubuntu <ubuntu@ip-172-31-36-188.us-west-2.compute.internal>
2013-07-31 04:02:52 +0000

    Changed to buffer read

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fileData = new Buffer(fs.readFileSync('index.html', 'utf8'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(fileData.toString());
 });
 
 var port = process.env.PORT || 5000;
